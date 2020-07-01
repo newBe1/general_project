8fa0b0374567f73dfb0a2f73700b29e0e91b92f1
zfrj2019/yanrui <2221595915@qq.com>
2020-07-02 06:21:49 +0800

    操作日志sql

--- sql/sys.sql
+++ sql/sys.sql
@@ -405,24 +405,22 @@ insert into sys_user_post values ('2', '2');
 -- ----------------------------
 -- 10、操作日志记录
 -- ----------------------------
-drop table if exists sys_oper_log;
-create table sys_oper_log (
-  oper_id           bigint(20)      not null auto_increment    comment '日志主键',
-  title             varchar(50)     default ''                 comment '模块标题',
-  business_type     int(2)          default 0                  comment '业务类型（0其它 1新增 2修改 3删除）',
-  method            varchar(100)    default ''                 comment '方法名称',
+drop table if exists operate_log;
+create table operate_log (
+  operate_id        bigint(20)      not null auto_increment    comment '日志主键',
+  module            varchar(50)     default ''                 comment '模块标题',
+  operate_type     int(2)          default 0                  comment '业务类型',
+  method_name            varchar(50)    default ''                 comment '方法名称',
   request_method    varchar(10)     default ''                 comment '请求方式',
-  operator_type     int(1)          default 0                  comment '操作类别（0其它 1后台用户 2手机端用户）',
-  oper_name         varchar(50)     default ''                 comment '操作人员',
-  dept_name         varchar(50)     default ''                 comment '部门名称',
-  oper_url          varchar(255)    default ''                 comment '请求URL',
-  oper_ip           varchar(50)     default ''                 comment '主机地址',
-  oper_location     varchar(255)    default ''                 comment '操作地点',
-  oper_param        varchar(2000)   default ''                 comment '请求参数',
+  operator_msg     varchar(100)         default 0                  comment '操作说明',
+  operate_name         varchar(50)     default ''                 comment '操作人员',
+  operate_url          varchar(255)    default ''                 comment '请求URL',
+  operate_ip           varchar(50)     default ''                 comment '主机地址',
+  operate_param        varchar(2000)   default ''                 comment '请求参数',
   json_result       varchar(2000)   default ''                 comment '返回参数',
   status            int(1)          default 0                  comment '操作状态（0正常 1异常）',
   error_msg         varchar(2000)   default ''                 comment '错误消息',
-  oper_time         datetime                                   comment '操作时间',
+  operate_time         datetime                                   comment '操作时间',
   primary key (oper_id)
 ) engine=innodb auto_increment=100 comment = '操作日志记录';
 
